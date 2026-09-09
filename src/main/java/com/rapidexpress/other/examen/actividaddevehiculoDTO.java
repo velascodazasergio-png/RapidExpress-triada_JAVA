784bdf552067e49bd7577c075af7f6deb4220460
Velascodazasergio-png <velascodazasergio@gmail.com>
2026-09-09 11:56:13 -0500

    feat: :memo: add the new  reports to the card

---
+++ src/main/java/com/rapidexpress/other/examen/actividaddevehiculoDTO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rapidexpress.model.dto;
+
+/**
+ *
+ * @author camper
+ */
+public record ActividadVehiculoDTO(
+        String placa,
+        String marca,
+        int rutasTotales,
+        int rutasFinalizadas,
+        int paquetesEntregados,
+        double kgEntregados,
+        double costoMantenimiento) {
+
+    /** Porcentaje de rutas que llegaron a Finalizada. */
+    public double porcentajeFinalizacion() {
+        return rutasTotales == 0 ? 0.0 : rutasFinalizadas * 100.0 / rutasTotales;
+    }
+}
+
+
